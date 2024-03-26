ee58e62fc759dd1080191331c3956bc4c8e72870
Jouini-Nour <nour.jouini.558@gmail.com>
2024-03-26 07:30:25 +0100

    établissement du connexion BD et du systéme de connexion

--- activitar-master/login.php
+++ activitar-master/login.php
@@ -1,3 +1,30 @@
+<?php 
+session_start();
+    require 'ConnexionBD.php';
+    $bdd = ConnexionBD::getInstance();
+    if(isset($_POST['login'])){
+        if(!empty($_POST['username']) && !empty($_POST['password'])){
+            $username = htmlspecialchars($_POST['username']);
+            $password = htmlspecialchars($_POST['password']);
+            $req = $bdd->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
+            $req->execute(array($username, $password));
+            if(($req->rowCount())== 1){
+                $user = $req->fetch(PDO::FETCH_ASSOC);
+                $_SESSION['id'] = $user['id'];
+                $_SESSION['username'] = $user['username'];
+                $_Session['password'] = $user['password'];
+                header('Location: index.php');
+            }else{
+                echo "Username ou mot de passe incorrect";
+            }
+            
+        }
+    }
+       
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,17 +40,17 @@
             <div class="title">
                 <span>Login</span>
             </div>
-            <form action="login.php" method="post">
+            <form action="login.php" method="POST">
                 <div class="row">
                     <i class="fa-solid fa-user"></i>
-                    <input type="text" placeholder="Username" required>
+                    <input type="text" placeholder="Username" name="username" required>
                 </div>
                 <div class="row">
                     <i class="fa-solid fa-lock"></i>
-                    <input type="password" placeholder="Password" required>
+                    <input type="password" placeholder="Password" name ="password" required>
                 </div>
                 <div class="row button">
-                    <input type="submit" value="Log in">
+                    <input type="submit" value="Log in" name="login">
                 </div>
                 <div class="signup-link">
                     Not a member? <a href="register.php">Signup now</a>
